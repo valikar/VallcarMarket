fd280d5353de04bb1b8cb2822c435c1a685d81a5
Emanuel Pruker <epruker@endava.net>
2017-04-10 12:12:33 +0300

    Add the generated metadata to the project.

---
+++ cmm/src/main/java/ro/cmm/CmmApplication.java
@@ -0,0 +1,12 @@
+package ro.cmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmmApplication.class, args);
+	}
+}
