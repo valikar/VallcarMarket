2cc5718224a94371e9ab804eee8d91020cbc675d
tamas-norbert-miklos <miklos.tamas.norbert@gmail.com>
2017-04-22 21:46:31 +0300

    CarService created with some basic funcionallity. Getters and setters generated in Car and CarLocation classes

--- src/main/java/ro/cmm/domain/CarLocation.java
+++ src/main/java/ro/cmm/domain/CarLocation.java
@@ -7,4 +7,20 @@ public class CarLocation {
 
     private long latitude;
     private long longtitude;
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+
+    public long getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(long longtitude) {
+        this.longtitude = longtitude;
+    }
 }
