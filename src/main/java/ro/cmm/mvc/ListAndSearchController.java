fa4f3dc2f5bb1b4d8920bbc9fe4aedd908181275
tamas-norbert-miklos <miklos.tamas.norbert@gmail.com>
2017-05-16 20:56:31 +0300

    Matriculation status added to search.ftl. Minor changes.

--- src/main/java/ro/cmm/mvc/ListAndSearchController.java
+++ src/main/java/ro/cmm/mvc/ListAndSearchController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ro.cmm.domain.Car;
@@ -13,8 +14,10 @@ import ro.cmm.domain.EngineType;
 import ro.cmm.domain.SearchModel;
 import ro.cmm.domain.TransmissionType;
 import ro.cmm.service.CarService;
+import ro.cmm.service.ValidationException;
 
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,7 +36,6 @@ public class ListAndSearchController {
     @RequestMapping("/search")
     public ModelAndView showSearch(SearchModel searchModel, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView("search");
-        Map<String,List<String>> map = carService.getManufacturersAndTypes();
 
         if (searchModel.getManufacturer() == null) {
             searchModel.setManufacturer("All");
@@ -44,6 +46,10 @@ public class ListAndSearchController {
         if (searchModel.getType() == null) {
             searchModel.setType("All");
         }
+        if (searchModel.getMatriculationStatus().size() == 0) {
+            searchModel.getMatriculationStatus().add(true);
+            searchModel.getMatriculationStatus().add(false);
+        }
         if (searchModel.getTransmissionType().size() == 0) {
             searchModel.getTransmissionType().add(TransmissionType.MANUAL);
             searchModel.getTransmissionType().add(TransmissionType.AUTOMATIC);
@@ -53,29 +59,46 @@ public class ListAndSearchController {
             searchModel.getEngineType().add(EngineType.PETROL);
         }
 
-        // if the user selects another manufacturer the form will submitForm automatically
-        // Lets say we have a scenario where the manufacturer and type options look like this:
-        //               Manufacturer: Ferrari        Type: LaFerrari
-        // Now if the user wants to choose Audi for the manufacturer
-        // the form will submitForm with the following values(because of autoSubmit)
-        //               Manufacturer: Audi        Type: LaFerrari
-        // to avoid this we must check if the types name is compatible with manufacturers name
-        // if not, the type will be set to "All" and all cars will be printed from the manufacturer
-        String searchModelManufacturer = searchModel.getManufacturer();
-        String searchModelType = searchModel.getType();
-
-        List<String> typeListForManufacturer = map.get(searchModelManufacturer);
-
-        if(!typeListForManufacturer.contains(searchModelType)) {
-            searchModel.setType("All");
+        List<String> errors = new LinkedList<>();
+        boolean hasErrors = false;
+        // because the fields: fabricationYear, price, mileAge are already initialized to 0
+        // a binding error occurrence does not affect the integrity of the search algorithm
+        // basically we can ignore these errors, BUT ONLY FOR THESE 3 FIELDS
+        // If other kind of binding error appears it will be handled correspondingly
+        if(bindingResult.hasErrors()) {
+            for (FieldError error: bindingResult.getFieldErrors()) {
+                if (!(error.getField().equalsIgnoreCase("fabricationYear") ||
+                        error.getField().equalsIgnoreCase("price") ||
+                        error.getField().equalsIgnoreCase("mileage"))) {
+                    errors.add(error.getField() + ":" + error.getCode());
+                    hasErrors = true;
+                }
+            }
         }
 
-        Collection<Car> results = carService.search(searchModel);
+        if(!hasErrors) {
+            try {
+                carService.validateSearchModel(searchModel);
+                Collection<Car> results = carService.search(searchModel);
+                modelAndView.addObject("results", results);
+
+            } catch (ValidationException e) {
+                errors = new LinkedList<>();
+                errors.add(e.getMessage());
+                modelAndView.addObject("results", new LinkedList<Car>());
+                modelAndView.addObject("errors", errors);
+                modelAndView.addObject("searchModel", searchModel);
+            }
+        } else {
+            modelAndView.addObject("results", new LinkedList<Car>());
+            modelAndView.addObject("errors", errors);
+            modelAndView.addObject("searchModel", searchModel);
+        }
+
+        Map<String,List<String>> map = carService.getManufacturersAndTypes();
 
         modelAndView.addObject("map", map);
         modelAndView.addObject("colours", carService.getAllColors());
-        modelAndView.addObject("results", results);
-        modelAndView.addObject("searchModel", searchModel);
 
         return modelAndView;
     }
